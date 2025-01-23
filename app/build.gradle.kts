4ddbeb935524b60aa3a93eb92d2ae78237eb0b84
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-01-23 16:33:32 -0500

    added viewmodel dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    // *------- added dependencies
+
+    // viewmodel
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
 }
