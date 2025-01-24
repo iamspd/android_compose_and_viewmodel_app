cac90808a4e630135062db6f8fcd1bf2118995b9
Priyank Shah <48063358+iamspd@users.noreply.github.com>
2025-01-24 18:29:08 -0500

    add unit tests for the game

--- app/src/test/java/com/example/scrambly/data/WordBank.kt
+++ app/src/test/java/com/example/scrambly/data/WordBank.kt
@@ -1,2 +1,26 @@
 package com.example.scrambly.data
 
+const val MAX_NO_OF_WORDS = 10
+const val SCORE_INCREASE = 20
+
+// set of all words for the test
+val wordBank: Set<String> = setOf(
+    "at",
+    "sea",
+    "home",
+    "arise",
+    "banana",
+    "android",
+    "birthday",
+    "briefcase",
+    "motorcycle",
+    "cauliflower"
+)
+
+/**
+ * Maps words to their lengths. Each word in wordBank has a unique length.
+ * This is done since the words are randomly picked inside GameViewModel and the
+ *      selection is unpredictable.
+ */
+private val wordLengthMap: Map<Int, String> = wordBank.associateBy({ it.length }, { it })
+internal fun getUnscrambledWord(scrambledWord: String) = wordLengthMap[scrambledWord.length] ?: ""
--- app/src/test/java/com/example/scrambly/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/scrambly/ui/test/GameViewModelTest.kt
@@ -1,2 +1,104 @@
 package com.example.scrambly.ui.test
 
+import com.example.scrambly.data.MAX_NO_OF_WORDS
+import com.example.scrambly.data.SCORE_INCREASE
+import com.example.scrambly.data.getUnscrambledWord
+import com.example.scrambly.ui.viewmodel.GameViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class GameViewModelTest {
+    private val gameViewModel = GameViewModel()
+
+    // test case for happy path
+    @Test
+    fun gameViewModel_CorrectWordGuessed_ScoreUpdatedAndErrorFlagUnset() {
+        var currentGameUiState = gameViewModel.gameUiState.value
+        val correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        gameViewModel.updateUserGuess(correctPlayerWord)
+        gameViewModel.checkUserGuess()
+
+        currentGameUiState = gameViewModel.gameUiState.value
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertEquals(SCORE_AFTER_FIRST_CORRECT_ANSWER, currentGameUiState.score)
+    }
+
+    // test case for error path
+    @Test
+    fun gameViewModel_IncorrectWordGuessed_ErrorFlagSetAndScoreNotUpdated() {
+        val incorrectPayerWord = "well"
+        gameViewModel.updateUserGuess(incorrectPayerWord)
+        gameViewModel.checkUserGuess()
+
+        val currentGameUiState = gameViewModel.gameUiState.value
+        assertTrue(currentGameUiState.isGuessedWordWrong)
+        assertEquals(INITIAL_SCORE, currentGameUiState.score)
+    }
+
+    // test case when app is launched
+    @Test
+    fun gameViewModel_Initialization_FirstWordLoaded() {
+        val currentGameUiState = gameViewModel.gameUiState.value
+        val unscrambledWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+        assertNotEquals(unscrambledWord, currentGameUiState.currentScrambledWord)
+        assertTrue(currentGameUiState.currentWordCount == 1)
+        assertTrue(currentGameUiState.score == 0)
+        assertFalse(currentGameUiState.isHintRequested)
+        assertFalse(currentGameUiState.isGuessedWordWrong)
+        assertFalse(currentGameUiState.isGameOver)
+    }
+
+    // test case when game is over with all correct words guessed
+    @Test
+    fun gameViewModel_AllWordsGuessedCorrectly_UIStateUpdatedCorrectly() {
+        var currentGameUiState = gameViewModel.gameUiState.value
+        var expectedScore = 0
+        var correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+
+        repeat(MAX_NO_OF_WORDS) {
+            expectedScore += SCORE_INCREASE
+            gameViewModel.updateUserGuess(correctPlayerWord)
+            gameViewModel.checkUserGuess()
+            currentGameUiState = gameViewModel.gameUiState.value
+            correctPlayerWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+            assertEquals(expectedScore, currentGameUiState.score)
+        }
+
+        assertEquals(MAX_NO_OF_WORDS, currentGameUiState.currentWordCount)
+        assertTrue(currentGameUiState.isGameOver)
+    }
+
+    // test case for the behavior of skip button
+    @Test
+    fun gameViewModel_WordSkipped_WordCountIncreasedAndScoreIsUnchanged() {
+        var currentGameUiState = gameViewModel.gameUiState.value
+        val wordCount = 1
+
+        gameViewModel.skipWord()
+        currentGameUiState = gameViewModel.gameUiState.value
+        assertEquals(wordCount.inc(), currentGameUiState.currentWordCount)
+        assertEquals(INITIAL_SCORE, currentGameUiState.score)
+    }
+
+    // test case when the player requests a hint
+    @Test
+    fun gameViewModel_PlayerRequestedAHint_HintFlagIsSetAndFirstLetterIsDisplayed() {
+        var currentGameUiState = gameViewModel.gameUiState.value
+        val unscrambledWord = getUnscrambledWord(currentGameUiState.currentScrambledWord)
+        val hintCharacter = unscrambledWord.substring(startIndex = 0, endIndex = 1)
+
+        gameViewModel.requestHint()
+        currentGameUiState = gameViewModel.gameUiState.value
+        assertTrue(currentGameUiState.isHintRequested)
+        assertEquals(hintCharacter, gameViewModel.showHintChar())
+    }
+
+    companion object {
+        private const val SCORE_AFTER_FIRST_CORRECT_ANSWER = 20
+        private const val INITIAL_SCORE = 0
+    }
+}
